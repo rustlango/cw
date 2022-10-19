4516558bb8fb07d50c2d77b4bc820472922508f5
Lemuel <wyntonsauls@gmail.com>
2022-10-19 11:13:32 +0200

    options1 completed - use if statement in match arm to handle before 10 pm condition

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -11,7 +11,7 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22
     // The Option output should gracefully handle cases where time_of_day > 24.
     match time_of_day {
-        0..22 => Some(5),
+        u16 if time_of_day < 22 => Some(5),
         22..= 24 => Some(0),
         _ => None,
     }
